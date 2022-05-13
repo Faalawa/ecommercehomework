04e2194802d210d4eacc315c8cc8116163b4cc95
Faalawa <99886608+Faalawa@users.noreply.github.com>
2022-05-13 09:41:49 -0400

    Add files via upload

--- App.js
+++ App.js
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Nav from './Nav';
 import './App.css';
+import ItemPage from './ItemPage';
+import {items} from './items-data';
 
-function App() {
+const App = () => {
+  const [activeTab, setActiveTab] = useState(' items ');
+  const [cart, setCart] = useState([]);
+  const addToCart = (item) => {
+    setCart([...cart, item]);
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Home</h1>
-        {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        
-      </header>
+      <Nav activeTab={activeTab} onTabChange={setActiveTab} />
+      <main className="App-content">
+        <content tab={activeTab} />
+        <span>Empty</span>
+      </main>
     </div>
   );
+};
+
+const Content = ({tab}) => {
+  switch (tab) {
+    case 'items':
+      return <ItemPage items={items} />;
+    case 'cart':
+      return <span>the cart</span>;
+    default:
+      break;
+  }
+  
 }
 
 export default App;
